9152bf8f111031790bc4b81e49aafe2e0a8d8e36
N1ghtStorm <diman1ghtly@gmail.com>
2021-08-07 13:10:29 +0300

    add actix

--- tracker_enricher/src/main.rs
+++ tracker_enricher/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+
+async fn main() -> std::io::Result<()> {
+    let url = "0.0.0.0:8001";
+
+    // START HTTP SERVER WITH GLOBAL STATE
+    HttpServer::new( move || {  
+        App::new()
+            .service(hi)
+    })
+    .bind(url)?
+    .run()
+    .await
+}
+
+/// Healthcheck endpoint
+#[get("/info")]
+async fn hi() -> impl Responder {
+    HttpResponse::Ok().body("
+        TRACKER ENRICHER RUNNING
+    ")
 }
