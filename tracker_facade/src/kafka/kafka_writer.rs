1d20f97a4947f372ccb5657ffcdb07e54388666b
N1ghtStorm <bushuevdv@mail.ru>
2021-08-16 22:39:42 +0300

    fix kafka problem

--- tracker_facade/src/kafka/kafka_writer.rs
+++ tracker_facade/src/kafka/kafka_writer.rs
@@ -10,43 +10,29 @@ use std::fmt::Write;
 use crate::{config, errors::KafkaSenderError, models::event::Event};
 
 pub fn send_event(event: Event) -> Result<(), KafkaSenderError>{
-    // let producer = get_producer();
-    // let message = event.get_serialized_string();
 
-    // let record = BaseRecord::to(config::APP_CONFIG.get_kafka_topic())
-    //                                         .key("1")
-    //                                         .payload(&message);
+    // TODO: REALLOC PROBLEM
+    // let broker = config::APP_CONFIG.get_kafka_brokers().clone();
+    
+    let mut producer = match get_producer() {
+                        Err(_) => return Err(KafkaSenderError{message: "error sending to kafka".to_string()}),
+                        Ok(p) => p
+                    };
 
-    // match producer.send(record) {
-    //     Err((ke, rec)) => Err(KafkaSenderError {message: "error sending to kafka".to_string()}),
-    //     Ok(()) => Ok(()) 
-    // }
+    let message = event.get_serialized_string();
 
-    let mut producer =
-    Producer::from_hosts(vec!("localhost:9092".to_owned()))
-                    .with_ack_timeout(Duration::from_secs(1))
-                    .with_required_acks(RequiredAcks::One)
-                    .create()
-                    .unwrap();
-
-    let buf = String::from("{ python pravit mirom }");
-
-    if let Err(e) = producer.send(&Record::from_value("facade-inbox", buf.as_bytes())) {
+    if let Err(e) = producer.send(&Record::from_value(config::APP_CONFIG.get_kafka_topic(), message.as_bytes())) {
         println!("{}", e);
         return Err(KafkaSenderError{message: "error sending to kafka".to_string()});
     }
-    // for _ in 0..10 {
-    //     //let _ = write!(&mut buf, "{}", i); // some computation of the message data to be sent
-    //     producer.send(&Record::from_value("facade-inbox", buf.as_bytes())).unwrap();
-    //     //buf.clear();
-    // }
 
     Ok(())
 }
 
-fn get_producer() -> BaseProducer {
-    ClientConfig::new()
-                .set("bootstrap.servers", config::APP_CONFIG.get_kafka_brokers())
-                .create()
-                .expect("invalid producer config")
+fn get_producer() -> Result<Producer, kafka::Error> {
+    // TODO: REALLOC PROBLEM
+    Producer::from_hosts(vec!(config::APP_CONFIG.get_kafka_brokers().clone()))
+                    .with_ack_timeout(Duration::from_secs(5))
+                    .with_required_acks(RequiredAcks::One)
+                    .create()
 }
