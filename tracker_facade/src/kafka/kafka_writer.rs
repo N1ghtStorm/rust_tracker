5e1e8cf1455beb693c0447b6f228246504832fcf
N1ghtStorm <bushuevdv@mail.ru>
2021-08-17 12:10:44 +0300

    add mutiple events processing

--- tracker_facade/src/kafka/kafka_writer.rs
+++ tracker_facade/src/kafka/kafka_writer.rs
@@ -3,6 +3,7 @@ use std::time::Duration;
 
 use crate::{config, errors::KafkaSenderError, models::event::Event};
 
+#[deprecated(since = "0.0", note = "Use batch sending")]
 pub fn send_event(event: Event) -> Result<(), KafkaSenderError>{
     let mut producer = match get_producer() {
                         Err(_) => return Err(KafkaSenderError{message: "error sending to kafka".to_string()}),
@@ -14,6 +15,25 @@ pub fn send_event(event: Event) -> Result<(), KafkaSenderError>{
     if let Err(e) = producer.send(&Record::from_value(config::APP_CONFIG.get_kafka_topic(), message.as_bytes())) {
         println!("{}", e);
         return Err(KafkaSenderError{message: "error sending to kafka".to_string()});
+    }
+
+    Ok(())
+}
+
+
+pub fn send_events(events_batch: Vec<Event>) -> Result<(), KafkaSenderError>{
+    let mut producer = match get_producer() {
+                        Err(_) => return Err(KafkaSenderError{message: "error creating kafka producer".to_string()}),
+                        Ok(p) => p
+                    };
+
+    let messages = Event::get_serialized_string_vec(events_batch);
+
+    for message in messages {
+        if let Err(e) = producer.send(&Record::from_value(config::APP_CONFIG.get_kafka_topic(), message.as_bytes())) {
+            println!("{}", e);
+            return Err(KafkaSenderError{message: "error sending to kafka".to_string()});
+        }
     }
 
     Ok(())
--- tracker_facade/src/models/event.rs
+++ tracker_facade/src/models/event.rs
@@ -16,6 +16,7 @@ pub struct Event {
 }
 
 impl Event {
+    /// Construct from eventdto
     pub fn new_from_event_dto(dto: EventDto) -> Result<Self, EventError> {
         let string_server_timestamp = chrono::offset::Local::now().to_string();
 
@@ -26,7 +27,43 @@ impl Event {
         Ok(Event {uuid: dto.uuid, action: dto.action, date: dto.date, button_name:dto.button_name, server_timestamp: string_server_timestamp})
     }
 
+    /// Construct events from eventdtos
+    pub fn new_eventvec_from_dto(dtos: Vec<EventDto>) -> Result<Vec<Self>, EventError> {
+        let string_server_timestamp = chrono::offset::Local::now().to_string();
+
+        // Check if uuids are valid
+        for dto in &dtos {
+            if let Err(_) = Uuid::parse_str(&dto.uuid) {
+                return Err(EventError{message: "error parsing uuid".to_string()});
+            }
+        }
+
+        let events = dtos.into_iter()
+                                    .map(|dto| {      
+                                        Event {
+                                            uuid: dto.uuid, 
+                                            action: dto.action, 
+                                            date: dto.date, 
+                                            button_name:dto.button_name, 
+                                            server_timestamp: string_server_timestamp.clone(),
+                                        }
+                                    })
+                                    .collect::<Vec<Self>>();
+        Ok(events)
+    }
+
+
+    // EVENT BY ITSELF MUST KNOW HOW TO BE SERIALIZED:
+
+    /// Get serialized string from single event
     pub fn get_serialized_string(&self) -> String {
         serde_json::to_string(&self).unwrap()
+    }
+
+    /// Get serialized strings from multiples events
+    pub fn get_serialized_string_vec(event_vec: Vec<Event>) -> Vec<String> {
+        event_vec.iter()
+                 .map(|x| x.get_serialized_string())
+                 .collect::<Vec<String>>()
     }
 }
